4cea6f64f1853515363bcd6fb5af8c0b5e223190
LearnerAnuprash <info.anuprash@gmail.com>
2025-11-09 08:39:20 +0545

    feat: functional dropdown for the 'Members' nav bar element

--- logicAnime.js
+++ logicAnime.js
@@ -1,14 +1,31 @@
-const navbar=document.getElementById("navbar");
+const navbar = document.getElementById("navbar");
 let scrollTimeout;
 
-window.addEventListener("scroll",()=>{
-   navbar.style.opacity="0.5";
+window.addEventListener("scroll", () => {
+  navbar.style.opacity = "0.5";
 
-   clearTimeout(scrollTimeout);
+  clearTimeout(scrollTimeout);
 
-   scrollTimeout=setTimeout(()=>
-   {
-    navbar.style.opacity="1";
-   },200);
+  scrollTimeout = setTimeout(() => {
+    navbar.style.opacity = "1";
+  }, 200);
+});
 
+const drop = document.querySelector("ul");
+const dropElements = document.querySelectorAll(".flexibleDropdown");
+const members = document.querySelector(".dropdown-menu");
+drop.style.display = "none";
+let dropCount = 1;
+members.addEventListener("click", () => {
+  if (dropCount % 2 == 0) {
+    drop.style.display = "none";
+    dropCount++;
+  } else {
+    drop.style.display = "block";
+    dropCount++;
+  }
+});
+
+dropElements.addEventListener("click", () => {
+  drop.style.display = "none";
 });
